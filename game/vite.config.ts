156ddd5a3b8b4148a867d2b170bfad9d04cb8745
Noah Alvarado <38107504+noah-alvarado@users.noreply.github.com>
2025-06-09 14:55:17 -0500

    test

--- game/vite.config.ts
+++ game/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import solidSvg from 'vite-plugin-solid-svg'
@@ -12,5 +15,18 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+  },
+  resolve: {
+    conditions: ['development', 'browser'],
+  },
+  test: {
+    environment: 'jsdom',
+    globals: false,
+    setupFiles: ['node_modules/@testing-library/jest-dom/vitest'],
+    isolate: false,
+    coverage: {
+      enabled: true,
+      include: ['src/**/*.{ts,tsx}'],
+    }
   },
 });
