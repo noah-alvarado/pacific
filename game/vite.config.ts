a5d9a70a065454df2dad7c9b6bcea44a10d8d554
Noah Alvarado <38107504+noah-alvarado@users.noreply.github.com>
2025-06-10 00:44:06 -0500

    env

--- game/vite.config.ts
+++ game/vite.config.ts
@@ -1,34 +1,43 @@
 /// <reference types="vitest" />
 /// <reference types="vite-plugin-solid-svg/types-component-solid" />
 /// <reference types="vite/client" />
+/// <reference types="@types/node" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+
 import solidPlugin from 'vite-plugin-solid';
 import solidSvg from 'vite-plugin-solid-svg'
 
-export default defineConfig({
-  base: import.meta.env.VITE_GITHUB_PAGES ? '/pacific/' : '/',
-  plugins: [
-    solidPlugin(),
-    solidSvg(),
-  ],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-  resolve: {
-    conditions: ['development', 'browser'],
-  },
-  test: {
-    environment: 'jsdom',
-    globals: false,
-    setupFiles: ['node_modules/@testing-library/jest-dom/vitest'],
-    isolate: false,
-    coverage: {
-      enabled: true,
-      include: ['src/**/*.{ts,tsx}'],
-    }
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    base: env.VITE_GITHUB_PAGES ? '/pacific/' : '/',
+    plugins: [
+      solidPlugin(),
+      solidSvg(),
+    ],
+    server: {
+      port: 3000,
+    },
+    build: {
+      target: 'esnext',
+    },
+    resolve: {
+      conditions: ['development', 'browser'],
+    },
+    test: {
+      environment: 'jsdom',
+      globals: false,
+      setupFiles: ['node_modules/@testing-library/jest-dom/vitest'],
+      isolate: false,
+      coverage: {
+        enabled: true,
+        include: ['src/**/*.{ts,tsx}'],
+      }
+    },
+  };
 });
